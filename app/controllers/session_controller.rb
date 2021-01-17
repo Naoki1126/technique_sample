becee03392847fd57cf2b295fe3acd05fe13ee23
NaokiMaruyama <m.naoki85@gmail.com>
2021-01-17 19:33:10 +0900

    apdate

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,13 +1,16 @@
 class SessionController < ApplicationController
     skip_before_action :verify_authenticity_token
     def read
+        if session[:message_count]
+            @test = session[:message_count]
+            raise
+        end
     end
 
     def save
         if params[:message_count]
             session[:message_count] = params[:message_count]
         end
-       
     end
 
     def destroy
