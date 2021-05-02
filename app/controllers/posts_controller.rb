23b45f9855f0a1e08a9ea7fba368a9632bde9dfa
NaokiMaruyama <m.naoki85@gmail.com>
2021-05-02 11:41:07 +0900

    update fuction of hashtag

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,22 +3,63 @@ class PostsController < ApplicationController
 
     def index
         @posts = Post.all
-        
+        @hashtags = Hashtag.all
+        post_hashtag = PostHashtag.all
     end
 
     def new
         @newpost = Post.new
-        @newpost.title = params[:title]
-        @newpost.caption = params[:caption]
 
     end
 
     def create
+        @newpost = Post.new
+        @newpost.title = params[:title]
+        @newpost.caption = params[:caption]
+
+        if @newpost.save
+            hashtag = extract_hashtag(@newpost.caption)
+            save_hashtag(hashtag)
+            redirect_to posts_path
+        end
 
     end
 
     def destroy
 
     end
+
+    private
+
+    #--------------ハッシュタグ保存処理　create アクションの中で実行　----------------
+    def extract_hashtag(caption)
+        if caption.blank? #例外処理のため。まずあり得ないが、引数が空で渡ってきた場合は処理をしない
+            return
+        end
+        # 入力された文字列の中より、＃で始まる文字列を配列にして返す
+        return caption.scan(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/) #=> ["#aaa","#bbb"]
+        
+    end
+
+    def save_hashtag(hashtag_array)
+
+        if hashtag_array.blank? #例外処理のため。ハッシュタグを付けずに投稿された時、下のメソッドを実行させないようにする。
+            return
+        end
+
+        hashtag_array.uniq.map do |hashtag|
+            # ハッシュタグは先頭の#を外し、小文字にして保存
+            tag = Hashtag.find_or_create_by(name: hashtag.downcase.delete('#'))
+            #-------中間テーブルへの保存処理--------
+            post_hashtag = PostHashtag.new #中間テーブルのインスタンスを作成
+            post_hashtag.post_id = @newpost.id 
+            post_hashtag.hashtag_id = tag.id
+        end
+    end
+
+    def post_params
+        params.require(:post).permit(:title, :caption, :user_id)
+      end
+    
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_29_101700) do
+ActiveRecord::Schema.define(version: 2021_05_02_024004) do
 
   create_table "csvtests", force: :cascade do |t|
     t.string "name"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2021_04_29_101700) do
     t.string "caption"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
