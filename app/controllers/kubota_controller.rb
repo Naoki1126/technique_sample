d0f4e1c8363b6849b3de5141ecc955edf2279f79
NaokiMaruyama <m.naoki85@gmail.com>
2021-06-19 20:28:09 +0900

    update

--- app/controllers/kubota_controller.rb
+++ app/controllers/kubota_controller.rb
@@ -10,21 +10,53 @@ class KubotaController < ApplicationController
 
     def map
         
-        if params[:address].include?("久保田の家")
-            @latlng = [36.57742818363322, 160.0319015]
-        else
-            results = Geocoder.search(params[:address])
-            # @latlng = results.first.coordinates
-            @latlng = [
-                [36.57742818363322, 120.0319015],
-                [24.9999,122.99999],
-                [36.55555,125.999990],
-                [38.44444444,28.9999999]
+        # if params[:address].include?("久保田の家")
+        #     @latlng = [36.57742818363322, 160.0319015]
+        # else
+        #     results = Geocoder.search(params[:address])
+        #     # @latlng = results.first.coordinates
+        #     @latlng = [
+        #         [36.57742818363322, 120.0319015],
+        #         [24.9999,122.99999],
+        #         [36.55555,125.999990],
+        #         [38.44444444,28.9999999]
+        #     ]
+        # end
+        # respond_to do |format|
+        #   format.js
+        # end
+        return_value = {
+            "data":[
+                {
+                    "place": "久保田の実家",
+                    "latitude": 36.57742818363322,
+                    "longitude": 120.0319015
+                },
+                {
+                    "place": "久保田りょう",
+                    "latitude":36.999,
+                    "longitude": 122.99999
+                },
+                {
+                    "place": "久保田という男",
+                    "latitude": 36.55555,
+                    "longitude": 120.0319015
+                },
+                {
+                    "place": "久保田という先生",
+                    "latitude": 36.57742818363322,
+                    "longitude": 125.999990
+                },
+                {
+                    "place": "久保田の昔の家",
+                    "latitude": 38.44444444,
+                    "longitude": 128.9999999
+                }
             ]
-        end
-        respond_to do |format|
-          format.js
-        end
+        }
+
+        render json: return_value
+
     end
 
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,8 +52,12 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
   # unless Rails.env.production?
-  config.web_console.whitelisted_ips = '0.0.0.0/0'
+  # config.web_console.whitelisted_ips = '0.0.0.0/0'
   # end
+
+  if Rails.env.deveropment?
+    config.web_console.whitelisted_ips = '0.0.0.0/0'
+    end
   # BetterErrors::Middleware.allow_ip! "0.0.0.0/0"
   # Raises error for missing translations
   config.action_view.raise_on_missing_translations = false
