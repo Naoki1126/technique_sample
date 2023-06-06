eeccf08640fb04e1751494238cf389d52182a97f
NaokiMaruyama <m.naoki85@gmail.com>
2023-06-06 13:44:48 +0900

    add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
 
+    protect_from_forgery with: :exception
+    
+    before_action :http_header_log
+    
+    private
+    
+    def http_header_log
+        logger.info("api_version:#{request.headers[:HTTP_API_VERSION]}")
+    end
+      
 end
